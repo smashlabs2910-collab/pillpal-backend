de3fc292ba24bdacff470037302822097527b5c6
TejaHarsha29 <tejaharsha321@gmail.com>
2025-09-13 02:33:52 +0530

    initial project setup

---
+++ src/main/java/com/pillpal/pillpalbackend/PillpalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pillpal.pillpalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillpalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillpalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pillpal/pillpalbackend/PillpalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pillpal.pillpalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillpalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
